8c6d71e187ba245084d739cd77f3539094b7cfa6
eucred00-dotcom <eucred00@gmail.com>
2025-12-28 17:18:26 +0200

    Add files via upload

---
+++ ranks.py
@@ -0,0 +1,11 @@
+RANK_ROLES = {
+    "iron": "Iron",
+    "bronze": "Bronze",
+    "silver": "Silver",
+    "gold": "Gold",
+    "platinum": "Platinum",
+    "diamond": "Diamond",
+    "ascendant": "Ascendant",
+    "immortal": "Immortal",
+    "radiant": "Radiant"
+}
